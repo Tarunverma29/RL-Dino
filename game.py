310db46f2b084a1c891227608c770241ce1b44d5
NavalParkipandala <parkipandlanaval1502@gmil.com>
2025-11-05 19:46:10 +0530

    this is my first commit

---
+++ game.py
@@ -0,0 +1,18 @@
+import pygame as pg
+pg.init()
+from pygame.locals import *
+import sys
+
+class Game:
+    def __init__(self):
+        self.win = pg.display.set_mode((600,300))
+        self.gameloop()
+
+    def gameloop(self):
+        while True:
+            for event in pg.event.get():
+                if event.type == QUIT:
+                    pg.quit()
+                    sys.exit()
+
+game = Game()
